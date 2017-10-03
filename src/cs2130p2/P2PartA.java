9730981780daa4b6aac1d97e764588212087e63f
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-10-02 21:17:52 -0600

    Formatted to match code style

--- src/cs2130p2/P2PartA.java
+++ src/cs2130p2/P2PartA.java
@@ -1,45 +1,40 @@
+//Program P2PartA.java
+//Function: f is the truth value of a logical proposition.
+
 package cs2130p2;
 
-//Program Proj2A.java
-//Function: f is the truth value of a logical proposition.
 import java.io.*;
 
-public class P2PartA
-{
-public static void main(String args[])
-{
+public class P2PartA {
+    public static void main(String args[]) {
 
 
-}
+    }
 
-private static char LProp(char p, char q, char r)
-{
-   // Logical expression
+    private static char LProp(char p, char q, char r) {
+       // Logical expression
+    
+    
+    }
 
+    private static char ANDlogic(char p, char q) {
+       // Logical AND function
+       char f = 'F';
+       if(p == 'T' && q == 'T')
+          f = 'T';
+       return f;
+    }
 
-}
+    private static char ORlogic(char p, char q) {
+       // Logical OR function
+    
+    
+    }
 
-private static char ANDlogic(char p, char q)
-{
-   // Logical AND function
-   char f = 'F';
-   if(p == 'T' && q == 'T')
-      f = 'T';
-   return f;
-}
-
-private static char ORlogic(char p, char q)
-{
-   // Logical OR function
-
-
-}
-
-private static char NOTlogic(char p)
-{
-   // Logical NOT function
-
-
-}
+    private static char NOTlogic(char p) {
+       // Logical NOT function
+    
+    
+    }
 
 } // end class
